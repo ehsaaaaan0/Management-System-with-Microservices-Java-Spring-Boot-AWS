b398b6e61a5bc79f8ac16f99e4d5a167e8e429fe
ehsana00 <a12346516@unet.univie.ac.at>
2026-03-30 01:09:24 +0200

    initalizing project

---
+++ patient-service/src/main/java/com/pm/patient_service/controller/PatientController.java
@@ -0,0 +1,28 @@
+package com.pm.patient_service.controller;
+
+import com.pm.patient_service.dto.PatientResponseDTO;
+import com.pm.patient_service.service.PatientService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<PatientResponseDTO>> getPatient(){
+        List<PatientResponseDTO> patients = patientService.getPatient();
+        return ResponseEntity.ok().body(patients);
+    }
+}
