b0bc53c14ed99832c08642968193c2c9e391370b
ehsana00 <a12346516@unet.univie.ac.at>
2026-04-08 01:25:43 +0200

    JWT TOken for login

---
+++ auth-service/src/main/java/com/pm/auth_service/service/AuthService.java
@@ -0,0 +1,28 @@
+package com.pm.auth_service.service;
+
+import com.pm.auth_service.dto.LoginRequestDTO;
+import com.pm.auth_service.dto.LoginResponseDTO;
+import com.pm.auth_service.model.User;
+import com.pm.auth_service.utils.JwtUtil;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuthService {
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+    private final UserService userService;
+    public AuthService(UserService userService, PasswordEncoder passwordEncoder, JwtUtil jwtUtil){
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
+    }
+    public Optional<String> authenticate(LoginRequestDTO loginRequestDTO){
+        Optional<String> token= userService.finalByEmail(loginRequestDTO.getEmail())
+                .filter(u-> passwordEncoder.matches(loginRequestDTO.getPassword(), u.getPassword()))
+                .map(u->jwtUtil.generateToken(u.getEmail(), u.getRole()));
+        return token;
+    }
+}
